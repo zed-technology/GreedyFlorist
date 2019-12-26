dd692498a9588b6e30d8b2b86d7774875f5595e0
Denys Klymenko <suncoin@gmail.com>
2019-12-26 10:12:21 +0200

    Initial commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the getMinimumCost function below.
+    static int getMinimumCost(int k, int[] c) {
+
+        Arrays.sort(c);
+
+        int minimumCost = 0;
+
+        int multiplicator = 0;
+        int i = c.length-1;
+        while (i>=0) {
+
+            for (int j = 0; j < k; j++) {
+                if (i >= 0) {
+                    int price = (multiplicator + 1) * c[i];
+                    minimumCost = minimumCost + price;
+                } else { break; }
+                i--;
+            }
+            multiplicator++;
+        }
+
+
+        return minimumCost;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int minimumCost = getMinimumCost(k, c);
+
+        bufferedWriter.write(String.valueOf(minimumCost));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
